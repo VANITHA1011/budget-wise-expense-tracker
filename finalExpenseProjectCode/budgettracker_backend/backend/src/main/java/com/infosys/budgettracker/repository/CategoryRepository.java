0f4b7f080581490560a9f4b3d74e466bc46da0a6
Vanitha N <vanithanallappan@gmail.com>
2025-12-27 05:02:35 +0530

    Removed duplicate backend folder completely

--- finalExpenseProjectCode/budgettracker_backend/backend/src/main/java/com/infosys/budgettracker/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-
-package com.infosys.budgettracker.repository;
-
-import com.infosys.budgettracker.model.CategoryEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {}
